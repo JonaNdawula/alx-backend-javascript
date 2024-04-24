76ed2456f880d40ca2831eef4473279043764e6e
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-24 12:39:35 -0700

    code update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,22 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: 'fulfilled', value: result.value };
-      } else {
-	return { status: 'rejected', reason: `Error: ${result.reason.message}` };
-      }
-    }));
+  const promise = [signUpUser(firstName, lastName),
+    uploadPhoto(fileName)];
+
+  return Promise.allSettled(promise)
+    .then((values) => {
+      const resolve = [];
+      values.forEach((value) => {
+	if (value.status === 'fulfiled') {
+	  resolve.push(value);
+	} else {
+	  resolve.push({
+	    status: value.status,
+	    value: `Error: ${value.reason.message}`,
+	  });
+	}
+      });
+      return resolve;
+    });
 }
