c8048653fc707718769c4fbd7d5a1fab75305ef6
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-24 11:56:01 -0700

    code update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,11 +2,8 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   const values = [uploadPhoto(), createUser()];
+  
   return Promise.all(values)
-    .then((values) => {
-      console.log(`${values[1].firstName}${values[1].lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`),
+      () => console.log('Signup system offline'));
 }
