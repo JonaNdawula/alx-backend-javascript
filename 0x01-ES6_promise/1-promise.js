2f323be4dfaa919b3f22c907c4d241cde1ff34c7
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-24 05:40:31 -0700

    code update

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,8 @@
 export default function getFullResponseFromAPI(success) {
   const aPromise = new Promise((res, rej) => {
-      // res is resolve
-      // rej is reject
-      if (success) {
+    // res is resolve
+    // rej is reject
+    if (success) {
       res({
         status: 200,
 	body: 'Success',
