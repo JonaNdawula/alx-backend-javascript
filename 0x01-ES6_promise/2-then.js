a6c667ce265f0b0c65228496f069a6d4cf6bba31
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-24 06:19:02 -0700

    codecode for 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromApi(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+	status: 200,
+	body: 'success',
+      };
+    })
+    .catch (() => new Error());
+}
