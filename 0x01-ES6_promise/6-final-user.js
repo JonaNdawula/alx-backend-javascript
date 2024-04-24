b16c26b3cf0d94e89c523aed6d9b790facfb0086
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-24 12:27:14 -0700

    code for 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+      } else {
+	return { status: 'rejected', reason: `Error: ${result.reason.message}` };
+      }
+    }));
+}
