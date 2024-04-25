3a84478e9e3be13677282f4237652cd11426aa2c
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-25 09:52:20 -0700

    code update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuatonWarningMessage');
+    if (new.target !== Building && this.evacuationWarningMessage
+      === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-
     this._sqft = sqft;
   }
 
