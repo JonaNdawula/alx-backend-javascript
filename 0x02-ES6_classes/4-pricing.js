5b5a924eb8999bbd09189a8dc0d91abe610c6e40
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-25 04:54:51 -0700

    code for 4. Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
