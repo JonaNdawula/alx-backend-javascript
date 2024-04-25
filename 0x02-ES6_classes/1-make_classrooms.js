642076fc49288dcd5cbaee8732e755987d1b659a
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-25 03:39:20 -0700

    update

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,5 @@
 import ClassRoom from './0-classroom';
 
-const arr = [19, 29, 34];
-
 export default function initializeRooms() {
-  return arr.map((numberOfStudents) => new ClassRoom(numberOfStudents));
+  return [19, 20, 34].map((numberOfStudents) => new ClassRoom(numberOfStudents));
 }
