d08d1822a46cedb63043ebb82b258ad953a13048
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-29 11:35:48 -0700

    code update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,12 +2,10 @@ const cleanSet = (set, startString) => {
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') {
     return '';
   }
-  const sets = [...set]
+  return [...set]
     .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length))
     .join('-');
-
-  return sets;
 };
 
 export default cleanSet;
