98ab62fde9bea4445d0176edab4d5642254a11f1
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-18 12:20:38 +0000

    code update for 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('readable', function() {
+process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
-  process.stdout.write(`Your name is: ${input}`);
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
