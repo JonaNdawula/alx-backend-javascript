48ced49a271067ce2afd120fc8d58f946b2ffd4b
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-18 12:08:44 +0000

    code update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,9 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', function() {
   const input = process.stdin.read();
-  console.log(`Your name is: ${input}`);  
+  if (input !== null) {
+  process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
 process.stdin.on('end', function() {
