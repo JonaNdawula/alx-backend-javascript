3fe5dc2b0e4d5cbf948796139187c1d63526e56f
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-19 12:36:54 -0700

    code update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -16,5 +16,15 @@ describe('calculateNumber', ()=>{
   it('should return 6 when inputs are 1.5 and 3.7', ()=>{
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-  
+  it('should return -4 when inputs are -1 and -3', () =>{
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+  });
+
+  it('should return -5 when inputs are -1 and -3.7', () =>{
+    assert.strictEqual(calculateNumber(-1, -3.7), -5);
+  });
+
+  it('should return -5 when inputs are -1.2 and -3.7', ()=>{
+    assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
+  });
 });
