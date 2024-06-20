aa1789f6f4f28c02029d764834e1bd89389c26f0
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-20 03:42:43 -0700

    code for 4. Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,21 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi =  require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should use Utils.calculateNumber for calculation', () =>{
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub.returns(10);
+   
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnceWith('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+});
